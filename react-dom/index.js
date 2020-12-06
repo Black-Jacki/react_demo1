9e7a36e15775a83926e9a09f7fed810f79d21170
Black-Jacki <745644582@qq.com>
2020-12-06 21:51:37 +0800

    React组件化实现方法

--- react-dom/index.js
+++ react-dom/index.js
@@ -1,18 +1,66 @@
+import Component from '../react/component'
+
 const ReactDOM = {
     render
 }
 
-// 设置属性
+// 渲染子节点
 function render(vnode, container) {
+    return container.appendChild(_render(vnode))
+}
 
-    if (vnode === undefined) { return }
+function createComponent(comp, props) {
+
+    let inst
+    // 如果是类定义的组件，则创建实例返回
+    if (comp.prototype && comp.prototype.render) {
+        inst = new comp(props)
+    } else {
+        // 如果是函数组件，将函数组件扩展成类组件，方便后面统一管理
+        inst = new Component(props)
+        inst.constructor = comp
+        // 定义render函数
+        inst.render = function() {
+            return this.constructor(props)
+        }
+    }
+
+    return inst
+}
+
+function renderComponent(comp) {
+    let base
+    const renderer =  comp.render()
+    base = _render(renderer)
+    comp.base = base
+}
+
+function setComponentProps(comp, props) {
+    // 设置组件的属性
+    comp.props = props
+    // 渲染组件
+    renderComponent(comp)
+}
+
+function _render(vnode) {
+    if (vnode === undefined || vnode === null || typeof vnode === 'boolean') {
+        vnode = ''
+    }
 
     // 如果vnode是字符串
     if (typeof vnode === 'string') {
         // 创建文本节点
-        const textNode = document.createTextNode(vnode)
+        return document.createTextNode(vnode)
+    }
 
-        return container.appendChild(textNode)
+    // 如果tag是函数，则渲染组件
+    if (typeof vnode.tag === 'function') {
+        // 1.创建组件
+        const comp = createComponent(vnode.tag, vnode.attrs)
+        // 2.设置组件的属性
+        setComponentProps(comp, vnode.attrs)
+        // 3.返回组件渲染的节点对象
+        return comp.base
     }
 
     // 如果是虚拟DOM对象
@@ -30,8 +78,7 @@ function render(vnode, container) {
     // 递归渲染子节点
     vnode.childrens.forEach(child => render(child, dom))
 
-    return container.appendChild(dom)
-
+    return dom
 }
 
 // 设置属性
@@ -52,7 +99,7 @@ function setAttribute(dom, key, value) {
             dom.style.cssText = value || ''
 
         } else if (value && typeof value === 'object') {
-            
+
             for (let k in value) {
                 if (typeof value[k] === 'number') {
                     dom.style[k] = value[k] + 'px'
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from '../react'
 import ReactDOM from '../react-dom'
 
-const ele = (
-    <div className="active" title="react">
-        hello, <span style={{ color: 'red', fontSize: 30 }}>react</span>
-    </div>
-)
 
-ReactDOM.render(ele, document.querySelector('#app'))
+const Home = () => {
+    return (
+        <div className="active" title="react">
+            hello, <span style={{ color: 'red', fontSize: 30 }}>react</span>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Home title={'active'} />,
+    document.querySelector('#app')
+)
